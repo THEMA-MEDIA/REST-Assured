64c603a4302c8d1636377ca3ddf15c2fcbd652c3
Theo van Hoesel <Th.J.v.Hoesel@THEMA-MEDIA.nl>
2015-12-07 15:15:43 +0100

    DBIx::Class::Resultset->deploy - now with data

--- scripts/deploy_users.pl
+++ scripts/deploy_users.pl
@@ -3,6 +3,17 @@ use MyData::Schema;
 my $schema = MyData::Schema
     ->connect('dbi:SQLite:dbname=../etc/MyData.sqlite', '', '');
 
-$schema->deploy;
+$schema->deploy( { sources => [ 'User' ], add_drop_table => 1 } );
+
+$schema->resultset('User')->populate([
+    {  user_id      => 1,
+       first_name   => 'John',
+       nick_name    => 'Doe',
+       email        => "john@doe.com"},
+    {  user_id      => 2,
+       first_name   => 'Mary',
+       nick_name    => 'Jane',
+       email        => "mary@.com"},
+]);
 
 __END__
