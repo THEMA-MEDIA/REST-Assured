2c23868a3ddc920446a027aafd4eaee5c3cbf5ae
Theo van Hoesel <Th.J.v.Hoesel@THEMA-MEDIA.nl>
2015-12-10 00:34:39 +0100

    /users/:uuid does Content Negotiation

--- lib/SimpleAPIUsers.pm
+++ lib/SimpleAPIUsers.pm
@@ -2,11 +2,9 @@ package SimpleAPIUsers;
 
 use Dancer2;
 use Dancer2::Plugin::DBIC;
-use Dancer2::Plugin::REST;
+use Dancer2::Plugin::HTTP::ContentNegotiation;
 
-prepare_serializer_for_format;
-
-get '/users.:format' => sub {
+get '/users' => sub {
     my $users;
     if (param 'name') {
         $users = resultset('User')
@@ -17,18 +15,24 @@ get '/users.:format' => sub {
     return [ map +{href=>'/users/'.$_->uuid, label=>$_->name}, $users->all ] ;
 };
 
-get '/users/:uuid.:format' => sub {
+get '/users/:uuid' => sub {
     my $user = resultset('User')->find( { uuid => params->{uuid} } );
     if ( !$user ) {
         status(404);
         return;
-    }    
-    return {
+    }
+    my $data = {
         uuid        => $user->uuid,
         name        => $user->name,
         nick_name   => $user->nick_name,
         email       => $user->email,
-    }
+    };
+    http_choose_media_type(
+        'application/json'   => sub { to_json   $data },
+        'text/x-yaml'        => sub { to_yaml   $data },
+        'text/x-data-dumper' => sub { to_dumper $data },
+        { default => undef }
+    );
 };
 
 1;
